22e9324e6967419182d4cac279d213dc8d26e593
sergioluis909090 <141997182+sergioluis909090@users.noreply.github.com>
2023-08-12 18:56:11 -0500

    aa

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const logInButton = document.getElementById('log_in');
+
+logInButton.addEventListener('click', () => {
+	const login_email = document.getElementById('login_email').value;
+	const login_pass = document.getElementById('login_pass').value;
+	if(login_email == 'eayala' && login_pass =='idat2011'){
+		alert("PAJERO DETECTADO !!! ERES EL REY DE LA PAJA NO CAMBIAS !!")
+	}
+});
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
